9ab328e433cbdbc5dce214e4951b14c73a44e765
marioxaxa <marioxaxa0@gmail.com>
2024-09-19 04:47:38 -0300

    feat: formulario de venda com o react hook form

--- client/src/components/NewSaleForm.tsx
+++ client/src/components/NewSaleForm.tsx
@@ -1,38 +1,48 @@
-import { Box, TextField, MenuItem } from "@mui/material";
-import React, { useState } from "react";
+import {
+    Box,
+    TextField,
+    MenuItem,
+    FormControl,
+    InputLabel,
+    Select,
+} from "@mui/material";
+import { useState } from "react";
 import estadosSiglas from "../utils/estadosSiglas";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 
 const estados = estadosSiglas();
 
+type InputsT = {
+    is_cash_payment: number | null;
+    discount: number | null;
+    extra: number;
+    state: string;
+};
+
 export default function NewSaleForm() {
-    // Estado para armazenar os valores do formulário
-    const [formData, setFormData] = useState({
-        is_cash_payment: "",
-        discount: "",
-        extra: "",
-        state: "",
+    const {
+        register,
+        handleSubmit,
+        control,
+        formState: { errors },
+    } = useForm<InputsT>({
+        defaultValues: {
+            is_cash_payment: null,
+            discount: null,
+            extra: -1,
+            state: "",
+        },
     });
 
-    // Função de callback para lidar com mudanças nos inputs
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: value,
-        });
-    };
-
-    // Função de callback para lidar com o envio do formulário
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // Aqui você pode manipular os dados do formulário como desejar
-        console.log("Formulário enviado com os dados: ", formData);
+    const onSubmit: SubmitHandler<InputsT> = (data) => {
+        console.log(data);
     };
 
     return (
         <Box
             component="form"
-            onSubmit={handleSubmit}
+            onSubmit={handleSubmit(onSubmit)}
             sx={{
                 display: "flex",
                 flexDirection: "column",
@@ -41,61 +51,74 @@ export default function NewSaleForm() {
                 margin: "auto",
             }}
         >
-            <TextField
-                label="Adicional de trabalho"
-                name="extra"
-                select
-                variant="outlined"
-                value={formData.extra}
-                onChange={handleChange}
-                required
-            >
-                <MenuItem value="admin">Padrão</MenuItem>
-                <MenuItem value="user">Turbo</MenuItem>
-                <MenuItem value="guest">Super Turbo</MenuItem>
-            </TextField>
+            <FormControl fullWidth>
+                <InputLabel id="extra-select-label">
+                    Adicional de trabalho
+                </InputLabel>
+                <Select
+                    labelId="extra-select-label"
+                    id="extra-select"
+                    label="Adicional de trabalho"
+                    {...register("extra", { required: "Campo obrigatorio" })}
+                    error={!!errors.extra}
+                >
+                    <MenuItem value={0}>Padrão</MenuItem>
+                    <MenuItem value={1}>Turbo</MenuItem>
+                    <MenuItem value={2}>Super Turbo</MenuItem>
+                </Select>
+            </FormControl>
 
-            <TextField
-                label="Selecione o destino"
-                name="state"
-                select
-                variant="outlined"
-                value={formData.state}
-                onChange={handleChange}
-                required
-            >
-                {estados.map((estado) => {
-                    return (
-                        <MenuItem value={estado.estado}>
-                            {estado.estado}
-                        </MenuItem>
-                    );
-                })}
-            </TextField>
+            <FormControl fullWidth>
+                <InputLabel id="state-select-label">
+                    Selecione o destino
+                </InputLabel>
+                <Select
+                    labelId="state-select-label"
+                    id="state-select"
+                    label="Selecione o destino"
+                    {...register("state", { required: "Campo obrigatorio" })}
+                >
+                    {estados.map((estado) => {
+                        return (
+                            <MenuItem value={estado.estado}>
+                                {estado.estado}
+                            </MenuItem>
+                        );
+                    })}
+                </Select>
+            </FormControl>
 
             <TextField
                 label="Desconto"
-                name="discount"
                 type="number"
                 variant="outlined"
-                value={formData.discount}
-                onChange={handleChange}
-                required
                 helperText={"O desconto máximo é de"}
+                {...register("discount", {
+                    required: "Campo obrigatorio",
+                    min: {
+                        value: 0,
+                        message: "O desconto minimo é 0",
+                    },
+                })}
             />
 
-            <TextField
-                label="Forma de pagamento"
-                name="is_cash_payment"
-                select
-                variant="outlined"
-                value={formData.is_cash_payment}
-                onChange={handleChange}
-                required
-            >
-                <MenuItem value="false">Cartão de credito</MenuItem>
-                <MenuItem value="true">Pix ou boleto</MenuItem>
-            </TextField>
+            <FormControl fullWidth>
+                <InputLabel id="cash-select-label">
+                    Forma de pagamento
+                </InputLabel>
+                <Select
+                    labelId="cash-select-label"
+                    id="cash-select"
+                    label="Forma de pagamento"
+                    {...register("is_cash_payment", {
+                        required: "Campo obrigatorio",
+                    })}
+                >
+                    <MenuItem value={0}>Cartão de credito</MenuItem>
+                    <MenuItem value={1}>Pix ou boleto</MenuItem>
+                </Select>
+            </FormControl>
+            <DevTool control={control} />
         </Box>
     );
 }
